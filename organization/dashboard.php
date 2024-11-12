681fec5b768bf3289ebcba1016d50977d7a9f310
AthenaMTC <169009435+AthenaMTC@users.noreply.github.com>
2024-11-13 00:34:00 +0800

    Admin/SignUp

---
+++ organization/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+  
+    require_once '../organization/includes/head.php';
+?>
+
+<body id="dashboard">
+    <div class="wrapper">
+        <?php
+            require_once '../organization/includes/topnav.php';
+            require_once '../organization/includes/sidebar.php';
+        ?>
+        <div class="content-page px-3">
+            <!-- dynamic content here -->
+        </div>
+    </div>
+    <?php
+        require_once '../organization/includes/footer.php';
+    ?>
+</body>
+</html>
