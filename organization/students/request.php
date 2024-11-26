d6188a1a0eadee1d99f0ae7a47e135e2f76e34f2
Ayana <ayanajadealejo@gmail.com>
2024-11-26 16:17:16 +0800

    records

---
+++ organization/students/request.php
@@ -0,0 +1,74 @@
+<div class="container-fluid">
+            <div class="table-container">
+            
+                <table class="table table-striped mt-4">
+                    <thead>
+                        <h4 class="mt-3">Refund Request (3)</h4>
+                    </thead>
+                    <tbody>
+                        <tr >
+                            <td class="status-unpaid"> [New]</td>
+                            <td>Alfaith Mae Luzon</td>
+                            <td style="color: #655E5E">Request ID : 12344</td>
+                            <td style="color: #655E5E">08:00 AM</td>
+                            <td style="color: #655E5E">Nov. 12, 2024</td>
+                            <td class="status-partial">Pending</td>
+                            <td>
+                                <div class="dropdown text-end border-0 bgreen">
+                                   <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
+                                      <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                           <i class="fas fa-ellipsis-v"></i>
+                                      </button>
+                                   </a>
+                                    <ul class="dropdown-menu text-small">
+                                       <a class="dropdown-item" href="#">View Details</a> 
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                       <tr>
+                            <td class="status-unpaid"></td>
+                            <td>Ayana Jade F. Alejo</td>
+                            <td style="color: #655E5E">Request ID : 12344</td>
+                            <td style="color: #655E5E">08:00 AM</td>
+                            <td style="color: #655E5E">Nov. 12, 2024</td>
+                            <td class="status-unpaid">Rejected </td>
+                            <td>
+                                <div class="dropdown text-end border-0 bgreen">
+                                   <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
+                                      <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                           <i class="fas fa-ellipsis-v"></i>
+                                      </button>
+                                   </a>
+                                    <ul class="dropdown-menu text-small">
+                                       <a class="dropdown-item" href="#">View Details</a> 
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="status-unpaid"></td>
+                            <td>Clark Falcatan</td>
+                            <td style="color: #655E5E">Request ID : 12d344</td>
+                            <td style="color: #655E5E">07:40 AM</td>
+                            <td style="color: #655E5E">Nov. 12, 2024</td>
+                            <td class="status-paid">Approved </td>
+                            <td>
+                                <div class="dropdown text-end border-0 bgreen">
+                                   <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
+                                      <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                           <i class="fas fa-ellipsis-v"></i>
+                                      </button>
+                                   </a>
+                                    <ul class="dropdown-menu text-small">
+                                       <a class="dropdown-item" href="#">View Details</a> 
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
