548658e80f739aff79713a5018b1a00df86ce5ed
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2024-11-29 14:06:01 +0800

    Continue College Admin UI

--- college/includes/head.php
+++ college/includes/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/college.css">
     <link rel="stylesheet" href="../vendor/bootstrap-5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../vendor//bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../vendor/datatable-2.1.8/datatables.min.css" >
