7ee8776cd792ddbcefad45ee4aac88b115a9f820
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2024-11-29 11:24:57 +0800

    unfisnished

---
+++ college/includes/sidebar.php
@@ -0,0 +1,56 @@
+<div class="sidebar">
+        <div class="sidebar-heading d-flex align-items-center justify-content-between">
+            <img src="../images/wmsu_logo.png" alt="CCS PayNet Logo" width="45">
+            <h4 class="ps-1"><span class="green"> WMSU </span> PayNet</h4>
+        </div>
+
+
+        <div class="sidebar-item">
+            <a href="dashboard" id="dashboard-link" class="nav-link">
+                <i class="bi bi-speedometer2"></i>
+                 Dashboard
+            </a>
+        </div>
+
+        <div class="sidebar-item">
+            <a href="" id="university-link" class="nav-link">
+                <i class="fa-solid fa-tent-arrow-left-right"></i>
+                University
+            </a>
+        </div>
+
+        <div class="sidebar-item">
+           <a href="" id="statistic-link" class="nav-link">
+                <i class="fa-solid fa-chart-simple"></i>
+                Statistics
+           </a>
+        </div>
+
+        <div class="sidebar-item" >
+            <a href="" id="audit-log-link" class="nav-link">
+                <i class="fa-solid fa-list"></i>
+                Audit & System
+            </a>
+        </div>
+
+        <div class="sidebar-item">
+            <a href="" id="backup-link" class="nav-link">
+                <i class="fa-solid fa-table"></i>
+                Backup & Data
+            </a>
+        </div>
+
+        <div class="sidebar-item" >
+            <a href="" id="feedback-link" class="nav-link">
+                <i class="fa-solid fa-people-group"></i>
+                User Feedback
+            </a>
+        </div>
+
+        <div class="sidebar-item" >
+            <a href="" id="maintenance-link" class="nav-link">
+                <i class="fa-solid fa-screwdriver-wrench"></i>
+                System Maintenance
+            </a>
+        </div>
+    </div>
