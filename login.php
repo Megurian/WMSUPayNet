26a906569923ebad04651c0209e54b44a3d12f62
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2024-12-02 01:00:29 +0800

    fix login

--- login.php
+++ login.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WMSU PayNet</title>
-    <link rel="stylesheet" href="../WMSUPayNet/css/login.css">
+    <link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
 
     <nav class="navbar">
         <div class="nav-logo">
-            <img src="../WMSUPayNet/images/wmsu_logo.png" alt="WMSU Logo">
+            <img src="/images/wmsu_logo.png" alt="WMSU Logo">
             <span>WMSU PayNet</span>
         </div>
     </nav>
     <div class="background">
         <div class="login-container">
             <div class="logo">
-                <img src="../WMSUPayNet/images/wmsu_logo.png" alt="WMSU Logo">
+                <img src="/images/wmsu_logo.png" alt="WMSU Logo">
             </div>
-            <form class="login-form" action="../WMSUPayNet/student/dashboard.html">
+            <form class="login-form" action="/student/dashboard.html">
                 <div class="form-group">
                     <label for="email">Email / Username</label>
                     <input type="text" id="email" name="email" required>
