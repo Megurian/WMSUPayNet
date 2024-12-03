53be9d2e18efdbc88d1b74602db4753c8bd4d436
AthenaMTC <169009435+AthenaMTC@users.noreply.github.com>
2024-12-04 00:59:55 +0800

    organization

---
+++ organization/reports/view-details.php
@@ -0,0 +1,118 @@
+<div class="container-fluid mt-3 mb-2">
+        <div class="row mb-3 border py-2 align-items-center">
+            <div class="col-md-1">
+                <a href="#" class="text-dark"><i class="fa fa-arrow-left"></i></a>
+            </div>
+            <div class="col-md-4">
+                <h4 class="mb-0" style="color: #093909 ;" >Weekly Payment Summary</h4>
+            </div>
+            <div class="col-md-7 text-right  align-items-center" >
+                <span > 
+                    <i class="fa fa-calendar-alt"></i> May 1, 2024 - May 7, 2024
+                </span>
+                <span class="mx-5">
+                    <i class="fa fa-university"></i> 1st Semester
+                </span>
+                <button id="download-report" class="btn bgreen btn-sm">Download Report</button>
+            </div>
+        </div>
+
+        <div class="d-flex mb-3">
+               <div class="d-flex align-items-center m-1">
+                    <select id="category-filter" class="form-select">
+                        <option value="choose">Course</option>
+                        <option value="">All</option>
+                        <option value="">Computer Science</option>
+                        <option value="">Information Tech</option>
+                        <option value="">Computer Techn</option>
+                    </select>
+                </div>
+
+                <div class="d-flex align-items-center m-1">
+                    <select id="category-filter" class="form-select">
+                        <option value="choose">Year Level</option>
+                        <option value="">All</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4</option>
+                    </select>
+                </div>
+
+        </div>
+        <div class="row border border-success">
+            <div class="col-md-12">
+                <table class="table border table-striped">
+                <thead>
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col">Student Name</th>
+                            <th scope="col">Course</th>
+                            <th scope="col">Year Level</th>
+                            <th scope="col">Fee</th>
+                            <th scope="col">Payment Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                        <tr>
+                            <td>May 1, 2024</td>
+                            <td>Falcatan, Clark Manuel F.</td>
+                            <td>Computer Science</td>
+                            <td>1</td>
+                            <td>Membership Fee</td>
+                            <td>₱ 50.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+       
+    </div>
---
+++ organization/settings/nav-bar.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+   
+    <div class="subnav">
+
+        <div class="subnav-item">
+            <a href="themes" id="themes-link" class="subnav-link"> Themes </a>
+        </div>
+
+        <div class="subnav-item">
+            <a href="account" id="account-link" class="subnav-link"> Account</a>
+        </div>
+
+        <div class="subnav-item">
+            <a href="roles" id="roles-link" class="subnav-link"> Roles
+            </a>
+        </div>
+
+    </div>
+</div>
