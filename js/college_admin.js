7014545193b0d28eb6ad622e12cc29a42f04a1df
AthenaMTC <169009435+AthenaMTC@users.noreply.github.com>
2024-12-03 20:15:34 +0800

    college Admin

--- js/college_admin.js
+++ js/college_admin.js
@@ -12,6 +12,7 @@ document.querySelectorAll('.sidebar-item a.nav-link').forEach(link => {
                 .then(html => {
                     document.querySelector('.content-page').innerHTML = html;
                     document.querySelector('.topnav-title').textContent = 'Dashboard';
+                    loadPie()
                 })
                 
         }else if (this.id === 'organizations-link') {
@@ -91,20 +92,94 @@ document.querySelectorAll('.sidebar-item a.nav-link').forEach(link => {
                 })
                 
         }else  if (this.id === 'fees-link') {
-            fetch('fees/fees.php')
+            fetch('fees/nav.php')
                 .then(response => response.text())
                 .then(html => {
                     document.querySelector('.content-page').innerHTML = html;
                     document.querySelector('.topnav-title').textContent = 'Fee Management';
 
-                    document.getElementById('view-fees').addEventListener('click', function(e) {
+                    document.querySelectorAll('.nav-item a.navi-link').forEach(link => {
+                        link.addEventListener('click', function(e) {
+                            e.preventDefault(); 
+                    
+                            document.querySelectorAll('.nav-item a.navi-link').forEach(link => link.classList.remove('link-active'));
+                            this.classList.add('link-active');
+
+                            if (this.id === 'organization-list-link') {
+                                fetch('fees/fees.php')
+                                    .then(response => response.text())
+                                    .then(html => {
+                                        document.querySelector('.table-content').innerHTML = html;
+                                       
+                                        
+                                        var table = $('#table-all').DataTable({
+                                            dom: 'rtp',
+                                            pageLength: 10,
+                                            ordering: false,
+                                        });
+                                        
+                                        
+                                    })
+                            }else if (this.id === 'college-link') {
+                                fetch('fees/college_fees.php')
+                                    .then(response => response.text())
+                                    .then(html => {
+                                        document.querySelector('.table-content').innerHTML = html;
+                                       
+                                        
+                                        var table = $('#table-all').DataTable({
+                                            dom: 'rtp',
+                                            pageLength: 10,
+                                            ordering: false,
+                                        });
+
+                                        document.getElementById('add-fee').addEventListener('click', function(e) {
+                                            e.preventDefault();
+                                            addFee();
+                                        });
+                                        
+                                    })
+                            }
+                        });
+                    })
+                    
+                    document.querySelector('.nav-item a#organization-list-link').click();
+                })
+                
+        }else  if (this.id === 'logs-link') {
+            fetch('logs/logs.php')
+                .then(response => response.text())
+                .then(html => {
+                    document.querySelector('.content-page').innerHTML = html;
+                    document.querySelector('.topnav-title').textContent = 'Activity Logs';
+
+                    
+                })
+                
+        }else  if (this.id === 'report-link') {
+            fetch('reports/report.php')
+                .then(response => response.text())
+                .then(html => {
+                    document.querySelector('.content-page').innerHTML = html;
+                    document.querySelector('.topnav-title').textContent = 'Report';
+
+                    document.getElementById('report-form').addEventListener('click', function(e) {
                         e.preventDefault();
-                        fetch('fees/view-fees.php')
+                        generateReport();
+                    });
+
+                    document.getElementById('view-report').addEventListener('click', function(e) {
+                        e.preventDefault();
+                        fetch('reports/view-report.php')
                         .then(response => response.text())
                         .then(html => {
                             document.querySelector('.content-page').innerHTML = html;
+                            document.querySelector('.topnav-title').textContent = 'Report';
+                            loadChart()
+                            viewDetails()
                         })
                     });
+                    
                 })
                 
         }
@@ -145,6 +220,19 @@ function addStudent() {
       });
   }
 
+  function addFee() {
+    fetch('fees/add-fee.html')
+      .then(response => response.text())
+      .then(html => {
+    
+        $('.modal-container').html(html);
+        $('#modal-add-fee').modal('show');
+        $('#form-add-fee').on('submit', function(e) {
+          e.preventDefault();
+        });
+      });
+  }
+
   function createAdmin() {
     fetch('organization/add-admin.html')
       .then(response => response.text())
@@ -157,7 +245,19 @@ function addStudent() {
         });
       });
   }
-
+  function generateReport() {
+    fetch('reports/generate_report.html')
+      .then(response => response.text())
+      .then(html => {
+    
+        $('.modal-container').html(html);
+        $('#modal-generate-report').modal('show');
+        $('#form-generate-report').on('submit', function(e) {
+          e.preventDefault();
+        });
+      });
+  }
+  
   function goBack() {
     window.history.back();
   }
@@ -195,3 +295,82 @@ function addStudent() {
         })
     });
   }
+
+  function loadChart(){
+    const chartData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Dataset 1',
+            data: [-500, -200, 600, 400, 700, 100, -100],
+            backgroundColor: 'rgba(9, 57, 9, 1)',
+            borderColor: 'rgba(9, 57, 9, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Dataset 2',
+            data: [-1200, 100, 700, 600, -200, -100, -100],
+            backgroundColor: 'rgba(160, 160, 160, 1)',
+            borderColor: 'rgba(160, 160, 160, 1)',
+            borderWidth: 1
+        }]
+    };
+
+    const paymentChart = document.getElementById('paymentChart').getContext('2d');
+    const myChart = new Chart(paymentChart, {
+        type: 'line',
+        data: chartData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+  }
+
+  function viewDetails(){
+    document.getElementById('view-details').addEventListener('click', function(e) {
+        e.preventDefault();
+        fetch('reports/view-details.php')
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('.content-page').innerHTML = html;
+            document.querySelector('.topnav-title').textContent = 'Report';
+        })
+    });
+  }
+
+  function loadPie(){
+    const paymentChartCanvas = document.getElementById('paymentChart');
+    const paymentChart = new Chart(paymentChartCanvas, {
+        type: 'pie',
+        data: {
+            labels: ['Full', 'Partial', 'Unpaid', 'Due'],
+            datasets: [{
+                label: 'Payment Status',
+                data: [70, 20, 5, 5], 
+                backgroundColor: [
+                    '#093909',
+                    '#84AE84',
+                    '#A0A0A0',
+                    '#EB1111'
+                ],
+                borderColor: [
+                    '#093909',
+                    '#84AE84',
+                    '#A0A0A0',
+                    '#EB1111'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+}
