535b2f5f154ef9dc8a6f5945827ceca81c6f89af
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2024-12-05 21:31:23 +0800

    fix to college_admin

--- js/college_admin.js
+++ js/college_admin.js
@@ -180,6 +180,13 @@ document.querySelectorAll('.sidebar-item a.nav-link').forEach(link => {
                         .then(response => response.text())
                         .then(html => {
                             document.querySelector('.content-page').innerHTML = html;
+                            document.querySelector('.topnav-title').textContent = 'Report';
+                            loadChart()
+                            viewDetails()
+                            document.getElementById('download-report').addEventListener('click', function(e) {
+                                e.preventDefault();
+                                downloadReport()
+                            });
                         })
                     });
                     
@@ -210,91 +217,176 @@ function addStudent() {
       });
   }
 
-  function addOrg() {
-    fetch('organization/add-organization.html')
-      .then(response => response.text())
-      .then(html => {
-    
-        $('.modal-container').html(html);
-        $('#modal-add-organization').modal('show');
-        $('#form-add-organization').on('submit', function(e) {
-          e.preventDefault();
-        });
-      });
-  }
+function addOrg() {
+fetch('organization/add-organization.html')
+    .then(response => response.text())
+    .then(html => {
 
-  function addFee() {
-    fetch('fees/add-fee.html')
-      .then(response => response.text())
-      .then(html => {
-    
-        $('.modal-container').html(html);
-        $('#modal-add-fee').modal('show');
-        $('#form-add-fee').on('submit', function(e) {
-          e.preventDefault();
-        });
-      });
-  }
-
-  function createAdmin() {
-    fetch('organization/add-admin.html')
-      .then(response => response.text())
-      .then(html => {
-    
-        $('.modal-container').html(html);
-        $('#modal-create-admin').modal('show');
-        $('#form-create-admin').on('submit', function(e) {
-          e.preventDefault();
-        });
-      });
-  }
-  function generateReport() {
-    fetch('reports/generate_report.html')
-      .then(response => response.text())
-      .then(html => {
-    
-        $('.modal-container').html(html);
-        $('#modal-generate-report').modal('show');
-        $('#form-generate-report').on('submit', function(e) {
-          e.preventDefault();
-        });
-      });
-  }
-  
-  function goBack() {
-    window.history.back();
-  }
-
-  function upload(){
-
-    const logoInput = document.getElementById('logo-input');
-    const logoPreview = document.getElementById('logo-preview');
-
-    logoInput.addEventListener('change', (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                logoPreview.src = e.target.result;
-                logoPreview.style.display = 'block';
-                document.querySelector('.plus-icon').style.display = 'none';
-            }
-            reader.readAsDataURL(file);
-        } else {
-            logoPreview.src = '#';
-            logoPreview.style.display = 'none';
-            document.querySelector('.plus-icon').style.display = 'block';
-        }
-    });
-  }
-
-  function viewFees(){
-    document.getElementById('org-overview-link').addEventListener('click', function(e) {
+    $('.modal-container').html(html);
+    $('#modal-add-organization').modal('show');
+    $('#form-add-organization').on('submit', function(e) {
         e.preventDefault();
-        fetch('fees/view-fees.php')
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector('.content-page').innerHTML = html;
-        })
     });
-  }
+    });
+}
+
+function addFee() {
+fetch('fees/add-fee.html')
+    .then(response => response.text())
+    .then(html => {
+
+    $('.modal-container').html(html);
+    $('#modal-add-fee').modal('show');
+    $('#form-add-fee').on('submit', function(e) {
+        e.preventDefault();
+    });
+    });
+}
+
+function createAdmin() {
+fetch('organization/add-admin.html')
+    .then(response => response.text())
+    .then(html => {
+
+    $('.modal-container').html(html);
+    $('#modal-create-admin').modal('show');
+    $('#form-create-admin').on('submit', function(e) {
+        e.preventDefault();
+    });
+    });
+}
+function generateReport() {
+fetch('reports/generate_report.html')
+    .then(response => response.text())
+    .then(html => {
+
+    $('.modal-container').html(html);
+    $('#modal-generate-report').modal('show');
+    $('#form-generate-report').on('submit', function(e) {
+        e.preventDefault();
+    });
+    });
+}
+
+function goBack() {
+window.history.back();
+}
+
+function upload(){
+
+const logoInput = document.getElementById('logo-input');
+const logoPreview = document.getElementById('logo-preview');
+
+logoInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            logoPreview.src = e.target.result;
+            logoPreview.style.display = 'block';
+            document.querySelector('.plus-icon').style.display = 'none';
+        }
+        reader.readAsDataURL(file);
+    } else {
+        logoPreview.src = '#';
+        logoPreview.style.display = 'none';
+        document.querySelector('.plus-icon').style.display = 'block';
+    }
+});
+}
+
+function viewFees(){
+document.getElementById('org-overview-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    fetch('fees/view-fees.php')
+    .then(response => response.text())
+    .then(html => {
+        document.querySelector('.content-page').innerHTML = html;
+    })
+});
+}
+
+function loadChart(){
+  const chartData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+          label: 'Dataset 1',
+          data: [-500, -200, 600, 400, 700, 100, -100],
+          backgroundColor: 'rgba(9, 57, 9, 1)',
+          borderColor: 'rgba(9, 57, 9, 1)',
+          borderWidth: 1
+      }, {
+          label: 'Dataset 2',
+          data: [-1200, 100, 700, 600, -200, -100, -100],
+          backgroundColor: 'rgba(160, 160, 160, 1)',
+          borderColor: 'rgba(160, 160, 160, 1)',
+          borderWidth: 1
+      }]
+  };
+  const paymentChart = document.getElementById('paymentChart').getContext('2d');
+  const myChart = new Chart(paymentChart, {
+      type: 'line',
+      data: chartData,
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+}
+function viewDetails(){
+  document.getElementById('view-details').addEventListener('click', function(e) {
+      e.preventDefault();
+      fetch('reports/view-details.php')
+      .then(response => response.text())
+      .then(html => {
+          document.querySelector('.content-page').innerHTML = html;
+          document.querySelector('.topnav-title').textContent = 'Report';
+      })
+  });
+}
+function loadPie(){
+  const paymentChartCanvas = document.getElementById('paymentChart');
+  const paymentChart = new Chart(paymentChartCanvas, {
+      type: 'pie',
+      data: {
+          labels: ['Full', 'Partial', 'Unpaid', 'Due'],
+          datasets: [{
+              label: 'Payment Status',
+              data: [70, 20, 5, 5], 
+              backgroundColor: [
+                  '#093909',
+                  '#84AE84',
+                  '#A0A0A0',
+                  '#EB1111'
+              ],
+              borderColor: [
+                  '#093909',
+                  '#84AE84',
+                  '#A0A0A0',
+                  '#EB1111'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          responsive: true,
+          plugins: {
+              legend: {
+                  position: 'bottom'
+              }
+          }
+      }
+  });
+}
+function downloadReport() {
+  fetch('reports/generate_report.html')
+    .then(response => response.text())
+    .then(html => {
+  
+      $('.modal-container').html(html);
+      $('#modal-download-report').modal('show');
+    });
+}
