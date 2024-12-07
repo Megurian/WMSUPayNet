fb2cc65ae6853f8a13183a24696224515496b016
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2024-12-07 15:05:20 +0800

    Display Students to table in College Admin

--- college/student/student.php
+++ college/student/student.php
@@ -1,3 +1,13 @@
+<?php
+    require_once '../../database/autoload_classes.php';
+
+    session_start();
+
+    $studentObj = new Students();
+    $suffixObj = new Suffixes();
+    $courseObj = new Courses();
+?>
+
 <div class="container-fluid mt-3">
 <div class="modal-container"></div>
         <div class="table-container">
@@ -31,41 +41,39 @@
                         <tr>
                             <th>Student ID</th>
                             <th class="sort-column" data-sort-by="name">Name <span class="sort-icon"></span></th>
-                            <th >Course</span></th>
-                            <th class="sort-column" data-sort-by="name">Year<span class="sort-icon"></th>
-                            <th >Email</th>
-                            <th >Phone Number</th>
-                            <th >Action</th>
+                            <th>Course</span></th>
+                            <th class="sort-column" data-sort-by="name">Year Level<span class="sort-icon"></th>
+                            <th style="display: flex; justify-content: center;">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>eh12345</td>
-                            <td> Luzon, Alfaith Mae </td>
-                            <td>Computer Science</td>
-                            <td>3rd Year</td>
-                            <td>Alfaith@example.com</td>
-                            <td>093-4556-7890</td>
-                            <td>
-                                <div class="dropdown text-end border-0 bgreen">
-                                    <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
-                                        <button class="btn bgreen dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v"></i>
-                                        </button>
-                                    </a>
-                                    <ul class="dropdown-menu text-small">
-                                        <a id="" class="dropdown-item" href="#">Edit</a> 
-                                        <a id="" class="dropdown-item" href="#">Delete</a> 
-                                        <a id="transaction" class="dropdown-item" href="#">View Details</a> 
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
+                        <?php foreach($studentObj->getStudentsbyCollegeId($_SESSION['account']['college_id']) as $student): ?>
+                            <tr>
+                                <td><?= htmlspecialchars($student['school_id']) ?></td>
+                                <td><?= htmlspecialchars($student['first_name'] . " " . $student['middle_name'] . " " . $student['last_name'] . " " . htmlspecialchars($suffixObj->getSuffixName($student['suffix_id']))) ?></td>
+                                <td><?= htmlspecialchars($courseObj->getCourseById($student['course_id'])) ?></td>
+                                <td><?= htmlspecialchars($student['year_level']) ?></td>
+                                <td style="display: flex; justify-content: center;">
+                                    <div class="dropdown text-end border-0 bgreen">
+                                        <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
+                                            <button class="btn bgreen dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="fas fa-ellipsis-v"></i>
+                                            </button>
+                                        </a>
+                                        <ul class="dropdown-menu text-small">
+                                            <a id="" class="dropdown-item" href="#">Edit</a> 
+                                            <a id="" class="dropdown-item" href="#">Delete</a> 
+                                            <a id="transaction" class="dropdown-item" href="#">View Details</a> 
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
 
             </div>
 
         </div>
-        
+
 </div>
