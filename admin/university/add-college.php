f2e053567289799c8029e735466acf2cb53fc1c5
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2025-02-01 01:40:26 +0800

    UX: Preview Uploaded Logo

--- admin/university/add-college.php
+++ admin/university/add-college.php
@@ -62,6 +62,23 @@ $suffixesObj = new Suffixes();
     </div>
 </div>
 
+<script>
+    //Script to preview uploaded logo
+    document.getElementById("logo-input").addEventListener("change", function(event) {
+        const file = event.target.files[0]; // Get the selected file
+        if (file) {
+            const reader = new FileReader(); // Create a FileReader object
+            reader.onload = function(e) {
+                const preview = document.getElementById("logo-preview");
+                preview.src = e.target.result; // Set the image source
+                preview.style.display = "block"; // Show the image preview
+                document.querySelector(".plus-icon").style.display = "none"; // Hide the plus icon
+            };
+            reader.readAsDataURL(file); // Read the file as a Data URL
+        }
+    });
+</script>
+
 <!-- Modal -->
 <div class="modal fade" id="modal-create-admin" tabindex="-1" aria-labelledby="modal-create-adminLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered ">
