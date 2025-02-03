da190d69af261d1668a389308d1e5d801610a35f
Athena Casiño <169009435+AthenaMTC@users.noreply.github.com>
2025-02-04 04:01:00 +0800

    system admin university revision

---
+++ admin/university/fees.php
@@ -0,0 +1,110 @@
+<div class="container-fluid">
+<div class="modal-container"></div>
+
+<h3 class="mt-4 mb-4"> University Fees</h3>
+            <div class="d-flex mb-3">
+                <div class="d-flex align-items-center m-1">
+                    <select id="category-filter" class="form-select">
+                        <option value="choose">Course</option>
+                        <option value="">All</option>
+                        <option value="">Computer Science</option>
+                        <option value="">Information Tech</option>
+                        <option value="">Computer Techn</option>
+                    </select>
+                </div>
+                <div class="d-flex align-items-center m-1">
+                    <select id="category-filter" class="form-select">
+                        <option value="choose">Year</option>
+                        <option value="">All</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4</option>
+                    </select>
+                </div>
+                <div class="d-flex align-items-center m-1">
+                    <select id="category-filter" class="form-select">
+                        <option value="choose">Semester</option>
+                        <option value="">All</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                    </select>
+                </div>
+            </div>
+
+            
+            <div class="table-container border">
+                    
+                    <div class="button mb-2 d-flex justify-content-end">
+                        <button id="add-fee" class="btn bgreen btn-sm"> Create Fee</button>
+                    </div>
+              <div class="table">
+                <table  class="table table-striped  mb-0">
+                    <thead>
+                        <tr>
+                            <th >Name</th>
+                            <th >Amount</th>
+                            <th >Course</th>
+                            <th >Year</th>
+                            <th >Semester</th>
+                            <th >Required</th>
+                            <th >Criteria</th>
+                            <th >School Year</th>
+                            <th ></th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Membership Fee</td>
+                            <td>$50.00</td>
+                            <td>All</td>
+                            <td>All</td>
+                            <td>1</td>
+                            <td>Yes</td>
+                            <td>All</td>
+                            <td>2023-2024</td>
+                            <td>
+                              <div class="dropdown text-end border-0 bgreen">
+                                    <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div class="text-black" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v"></i>
+                                        </div>
+                                    </a>
+                                      <ul class="dropdown-menu text-small">
+                                        <a id="" class="dropdown-item" href="#">Edit</a> 
+                                        <a id="" class="dropdown-item" href="#">Delete</a> 
+                                      </ul>
+                                  </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Miscellaneous</td>
+                            <td>$20.00</td>
+                            <td>All</td>
+                            <td>All</td>
+                            <td>1</td>
+                            <td>Yes</td>
+                            <td>All</td>
+                            <td>2023-2024</td>
+                            <td>
+                              <div class="dropdown text-end border-0 bgreen">
+                                    <a href="#" class="box " data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div class="text-black" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v"></i>
+                                        </div>
+                                    </a>
+                                      <ul class="dropdown-menu text-small">
+                                        <a id="" class="dropdown-item" href="#">Edit</a> 
+                                        <a id="" class="dropdown-item" href="#">Delete</a> 
+                                      </ul>
+                                  </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+ 
+            </div>   
+            </div>
+        </div>
