8a98b98ac001829415254daeb7896eb0365d2003
Meg Ryan Gomez <123749548+Megurian@users.noreply.github.com>
2025-02-18 19:05:19 +0800

    Update: Global Call for Logo Preview Script

--- admin/university/add-college.php
+++ admin/university/add-college.php
@@ -70,22 +70,8 @@ $suffixesObj = new Suffixes();
     </div>
 </div>
 
+<script src="../../js/UX.js"></script>
 <script>
-    //Script to preview uploaded logo
-    document.getElementById("logo-input").addEventListener("change", function(event) {
-        const file = event.target.files[0]; // Get the selected file
-        if (file) {
-            const reader = new FileReader(); // Create a FileReader object
-            reader.onload = function(e) {
-                const preview = document.getElementById("logo-preview");
-                preview.src = e.target.result; // Set the image source
-                preview.style.display = "block"; // Show the image preview
-                document.querySelector(".plus-icon").style.display = "none"; // Hide the plus icon
-            };
-            reader.readAsDataURL(file); // Read the file as a Data URL
-        }
-    });
-
     //Real=time validation college name format and generate college code
     document.getElementById("collegeName").addEventListener("input", function(event) {
         const collegeName = document.getElementById("collegeName");
